3417933c76b8700b8bc22b703bd44ff33d1590dd
starichockmoh <an.yan.7210@gmail.com>
2022-02-22 19:45:59 +0400

    change function fib to iter method

--- fib.py
+++ fib.py
@@ -1,10 +1,13 @@
 def fib(n):
-    if n == 1:
-        return 0
-    if n in (2, 3):
-        return 1
-    return fib(n - 1) + fib(n - 2)
+    def fibIter(a, b, count):
+        if count == 0:
+            return b
+        else:
+            print(a)
+            return fibIter(a + b, a, count - 1)
+
+    return fibIter(0, 1, n)
 
 
 x = int(input())
-print(fib(x))
+fib(x)
